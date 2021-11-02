034e86f71e5662a9dffdfb56e0a8f590e936d2c7
FELIXDAD <a2241119417@163.com>
2021-11-03 02:10:13 +0800

    learn

---
+++ 01 C++PrintHelloWorld/18.算数运算符_前置递增后置递增.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+#include <string>
+int main()
+{
+	   
+	//前置递增
+	int a = 10;
+	++a;
+	cout << a << endl;
+
+	//后置递增
+	int b = 10;
+	b++;
+	cout << b << endl;
+
+	//前置和后置的区别 
+	//前置递增 先让变量 +1 在进行表达式运算
+	int a2 = 10;
+	int b2 = ++a2 * 10;
+	cout << a2 << endl;
+	cout << b2 << endl;
+
+	//后置递增 先让变量 +1 在进行表达式运算
+	int a3 = 10;
+	int b3 = a3++ * 10;		//后置是在a3参与完运算后 再把数值加给a3
+	cout << a3 << endl;		//11
+	cout << b3 << endl;		//100
+	system("pause"); 
+
+	return 0;
+
+
+
+}
